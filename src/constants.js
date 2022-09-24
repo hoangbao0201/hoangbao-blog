b3ed4fea26fe9b78bbcf218cbd1d0b5aee65a308
Nguyễn Hoàng Bảo <hoangbao020103@gmail.com>
2022-09-24 21:43:13 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,2 @@
-// export const apiUrl = "https://hoangbao-blog.herokuapp.com" || ;
-
-
-export const apiUrl =
-	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000'
-		: 'https://hoangbao-blog.herokuapp.com'
-
+export const apiUrl = "https://hoangbao-blog.herokuapp.com";
 export const LOCAL_STORAGE_TOKEN_NAME = "TokenUser";
