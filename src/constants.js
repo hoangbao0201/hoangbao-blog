66d374f8ebd7f2a6e5fae478bf42fbbca06c822b
hoangbao0201 <hoangbao020103@gmail.com>
2022-09-24 21:35:08 +0700

    BE

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
-export const apiUrl = "http://localhost:5000";
+// export const apiUrl = "https://hoangbao-blog.herokuapp.com" || ;
+
+
+export const apiUrl =
+	process.env.NODE_ENV !== 'production'
+		? 'http://localhost:5000'
+		: 'https://hoangbao-blog.herokuapp.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = "TokenUser";
